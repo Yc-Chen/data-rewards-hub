1689dd5faa666a3007820d413feb0b119bd6e81f
Yicong Chen <chen.yicong@outlook.com>
2023-11-24 22:34:10 +0100

    basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,44 @@
 <script setup>
+import { onMounted } from 'vue';
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
+onMounted(() => {
+  console.log('mounted!')
+})
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <sl-button size="small">Click me</sl-button>
+    <div class="header-content">
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="40" height="40" />
+      <h1>Data Rewards Hub</h1>
+      <div style="flex-grow: 1"></div>
+      <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <sl-button>Click me</sl-button>
   </main>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
+  border-bottom: 1px solid var(--color-border);
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.header-content {
+  color: var(--color-heading);
+  display: flex;
+  place-items: center;
+  justify-content: space-between;
+  gap: var(--size-1);
+  padding: var(--size-3);
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+main {
+  padding: var(--size-3);
 }
 </style>
